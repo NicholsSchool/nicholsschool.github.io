8615b84090f372e7668147cccc0d0c75dfecce26
Arnav100 <parashararnav2@gmail.com>
2019-06-29 13:57:37 -0400

    Working logo carousel

--- slider.js
+++ slider.js
@@ -1,32 +1,32 @@
 document.addEventListener("DOMContentLoaded", event => {
 
-    $('#test').on("click", function () {
-        var logo1 = $('#logo1').offset();
-        var logo2 = $('#logo2').offset();
-        var logo3 = $('#logo3').offset();
+    // $('#test').on("click", function () {
+    //     var logo1 = $('#logo1').offset();
+    //     var logo2 = $('#logo2').offset();
+    //     var logo3 = $('#logo3').offset();
 
-        // $('#logo1').css('left', logo1.left).css('top', logo1.top);
-        // $('#logo2').css('left', logo2.left).css('top', logo2.top);
-        // $('#logo3').css('left', logo3.left).css('top', logo3.top);
-        var oneToThree = logo1.left - logo3.left;
-        var twoToOne = logo2.left - logo1.left;
-        var threeToTwo = logo3.left - logo2.left;
-        var twoTop = logo2.top - $("#main-image").height() + 8;
-        var oneTop = logo1.top - $("#main-image").height() - 16;
-        console.log("OneTop: " + oneTop + "  twoTop: " + twoTop);
-        console.log("two to one" + twoToOne);
-        console.log("One to three: "  + logo3.left);
-        $("#logo2").animate({ left: -twoToOne, top: -oneTop}, 2500);
-        $("#logo3").animate({ left: -threeToTwo, top: twoTop }, 2500);
-        $('#logo1').animate({ left: "150%" }, 1250, function () {
-            $(this).animate({ left: '80%' });
-        });
-        // $("#logo1").animate({ left: '-150%' }, 1250, function(){
-        //     $(this).animate({ left: '150%' }, function (){
+    //     // $('#logo1').css('left', logo1.left).css('top', logo1.top);
+    //     // $('#logo2').css('left', logo2.left).css('top', logo2.top);
+    //     // $('#logo3').css('left', logo3.left).css('top', logo3.top);
+    //     var oneToThree = logo1.left - logo3.left;
+    //     var twoToOne = logo2.left - logo1.left;
+    //     var threeToTwo = logo3.left - logo2.left;
+    //     var twoTop = logo2.top - $("#main-image").height() + 8;
+    //     var oneTop = logo1.top - $("#main-image").height() - 16;
+    //     console.log("OneTop: " + oneTop + "  twoTop: " + twoTop);
+    //     console.log("two to one" + twoToOne);
+    //     console.log("One to three: "  + logo3.left);
+    //     $("#logo2").animate({ left: -twoToOne, top: -oneTop}, 2500);
+    //     $("#logo3").animate({ left: -threeToTwo, top: twoTop }, 2500);
+    //     $('#logo1').animate({ left: "150%" }, 1250, function () {
+    //         $(this).animate({ left: '80%' });
+    //     });
+    //     // $("#logo1").animate({ left: '-150%' }, 1250, function(){
+    //     //     $(this).animate({ left: '150%' }, function (){
                 
-        //     });
-        // });
-    })
+    //     //     });
+    //     // });
+    // })
    
     console.log(logo1.left);
     console.log(logo2.left);
